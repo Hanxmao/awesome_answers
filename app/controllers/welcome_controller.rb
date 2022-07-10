c5dca17efea24cbc8811388a59b6798761805161
han <hanxmao@hotmail.com>
2022-07-09 21:12:41 -0700

    rails basic

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,11 @@
+class WelcomeController < ApplicationController
+    def index
+    end
+
+    def form_example
+    end
+
+    def about
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'question/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get('/welcome', {to: 'welcome#index'}) 
+  get '/about' => 'welcome#about'
+  get('/form_example', {to: 'welcome#form_example'})
 end
